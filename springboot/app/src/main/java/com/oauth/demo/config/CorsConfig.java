57e41eaa54829160f9a1cf9bfed0b7335ce56217
unknown <hs22770148@myu.universitecentrale.tn>
2026-03-05 09:45:56 +0100

    fix 3 files

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Add your allowed origins here
+                .allowedOrigins("http://127.0.0.1:4200") // Add your allowed origins here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
